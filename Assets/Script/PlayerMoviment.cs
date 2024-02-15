e7e1488f26e546ab1e7581bfe877b39feb2908ba
Latanks <76797064+LAtanks@users.noreply.github.com>
2024-02-15 16:59:04 -0300

    1.1

--- Assets/Script/PlayerMoviment.cs
+++ Assets/Script/PlayerMoviment.cs
@@ -1,13 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerMoviment : MonoBehaviour
 {
     [SerializeField]private BoxCollider2D collider2d;
-    [SerializeField]private SpriteRenderer skin;
+    [SerializeField]private Rigidbody2D rb;
     [SerializeField]private float walkSpeed;
     [SerializeField]private float runSpeed;
+    [SerializeField]private float rotationSpeed;
+    //dash
+    [SerializeField]private Slider dashLoadUI;
+    [SerializeField]private float time;
+    [SerializeField]private float timeMultiplier;
+    [SerializeField]private float dashForce;
 
 
     //Camera
@@ -16,9 +24,12 @@ public class PlayerMoviment : MonoBehaviour
     bool isLocked = true;
     float speed;
     Vector2 moviment;
+    Vector2 smoothMoviment;
+    Vector2 smoothMovimentVelocity;
     void Start()
     {
         collider2d = GetComponent<BoxCollider2D>();    
+        rb = GetComponent<Rigidbody2D>();
         speed = walkSpeed;
     }
 
@@ -29,20 +40,22 @@ public class PlayerMoviment : MonoBehaviour
             Moviment();
             Camera();
             Rotation();
+            Dash();
         }
     }
     void Moviment(){
-        moviment = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")) * speed * Time.deltaTime;
-
+        moviment = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        
         if(Input.GetKey(KeyCode.LeftShift)) speed = runSpeed; else speed = walkSpeed;
 
-        transform.position += transform.right * moviment.x + transform.up * moviment.y;
+        //transform.position += transform.right * moviment.x + transform.up * moviment.y;
+        rb.velocity = moviment  * speed;
     }
 
     public void Rotation(){
         if(moviment != Vector2.zero){
             Quaternion targetRotation = Quaternion.LookRotation(transform.forward, moviment);
-            Quaternion rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, 2 * Time.deltaTime);
+            Quaternion rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
 
             transform.rotation = rotation;
         }
@@ -56,5 +69,20 @@ public class PlayerMoviment : MonoBehaviour
         float yNew = Mathf.Lerp(cam.transform.position.y, yTarget, Time.deltaTime * camSmoothing);
 
         cam.transform.position = new Vector3(xNew, yNew, cam.transform.position.z);
+    }
+
+    public void Dash(){
+        if(Input.GetKey(KeyCode.E)){
+            if(time <= 100 )time += timeMultiplier * Time.deltaTime;
+        }
+
+        dashLoadUI.value = time;
+
+        if(Input.GetKeyUp(KeyCode.E)) {
+            rb.AddForce(transform.up * (dashForce * time));
+            time = 0;
+            dashLoadUI.value = 0;
+        }
+
     }
 }
